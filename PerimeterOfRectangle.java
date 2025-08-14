8c5f82e4a4144726ce880fa64fe3c695eb31ec60
Charvi Mittal <charvimittal304@gmail.com>
2025-08-14 11:29:37 +0530

    [charvi] Added Perimeter of a Rectangle code

---
+++ PerimeterOfRectangle.java
@@ -0,0 +1,8 @@
+public class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        int l=2;
+        int w=5;
+        int p=2*(l+w);
+        System.out.println(p);
+    }
+}
