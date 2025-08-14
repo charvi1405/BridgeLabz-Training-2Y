61e56e5e11cd16794b52d6d04850ccc6d3031bde
Charvi Mittal <charvimittal304@gmail.com>
2025-08-14 11:32:33 +0530

    [charvi] Added Power Calculation code

---
+++ PowerCalculation.java
@@ -0,0 +1,9 @@
+public class PowerCalculation {
+    public static void main(String[] args) {
+        int base = 2;
+        int exponent = 3;
+        double result = Math.pow(base, exponent);
+        System.out.println("The result of " + base + " raised to the power of " + exponent + " is: " + result);
+
+    }
+}
