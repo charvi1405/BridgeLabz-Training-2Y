09fa1574022a00c909b01e7f3a2f5529638c13cf
Charvi Mittal <charvimittal304@gmail.com>
2025-08-14 11:22:11 +0530

    [charvi] Added Volume Of a Cylinder

---
+++ VolumeOfCylinder.java
@@ -0,0 +1,10 @@
+public class VolumeOfCylinder {
+    public static void main(String[] args) {
+        int r=3;
+        int h=5;
+        double v= 3.14f*r*r*h;
+        System.out.println(v);
+
+    }
+    
+}
