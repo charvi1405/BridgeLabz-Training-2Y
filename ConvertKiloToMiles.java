8024d501c994f7d329ffd2b7fb402fe67aec0b22
Charvi Mittal <charvimittal304@gmail.com>
2025-08-14 11:38:02 +0530

    [charvi] added convert KIlo to Miles code

---
+++ ConvertKiloToMiles.java
@@ -0,0 +1,9 @@
+public class ConvertKiloToMiles {
+    public static void main(String[] args) {
+        double kilometers = 10.0;
+        double miles = kilometers * 0.621371;
+        System.out.println(miles);
+
+    }
+    
+}
