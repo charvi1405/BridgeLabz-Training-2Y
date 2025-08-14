b73fa317230c8244a7c6ba1b74f09e1a752ab634
Charvi Mittal <charvimittal304@gmail.com>
2025-08-14 11:26:59 +0530

    [charvi] Added Simple Interest code

---
+++ SI.java
@@ -0,0 +1,9 @@
+public class SI {
+    public static void main(String[] args) {
+        int p=12;
+        int r=2;
+        int t=5;
+        float SI=(p*r*t)/100.0f;
+        System.out.println(SI);
+    }
+}
