847812ad8eb752ea16ebccbf8748ad3b9c049ced
Charvi Mittal <charvimittal304@gmail.com>
2025-08-14 11:35:28 +0530

    [charvi] Added Calculate average of 3 no

---
+++ Average.java
@@ -0,0 +1,12 @@
+public class Average {
+    public static void main(String[] args) {
+        double num1 = 10.5;
+        double num2 = 20.5;
+        double num3 = 30.5;
+
+        double average = (num1 + num2 + num3) / 3;
+
+        System.out.println(average);
+    }
+    
+}
