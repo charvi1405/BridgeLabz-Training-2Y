35eb61ee18a1f16d5e500fe0403e6f1ba6a6154f
Charvi Mittal <charvimittal304@gmail.com>
2025-08-22 13:22:33 +0530

    [charvi] Added code

---
+++ Programming-Elements/Level-2/TotalPrice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TotalPrice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the unit price (INR): ");
+        int unitPrice = scanner.nextInt();
+        System.out.print("Enter the quantity: ");
+        int quantity = scanner.nextInt();        
+        double totalPrice = unitPrice * quantity;
+        System.out.println("The total purchase price is INR " + totalPrice + " if the quantity " + quantity + " and unit price is INR " + unitPrice);
+
+        scanner.close();
+}}
