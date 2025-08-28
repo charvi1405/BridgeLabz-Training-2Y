2d41e6811d1fafe33af8137d27611bc689eb1097
Charvi Mittal <charvimittal304@gmail.com>
2025-08-28 12:08:21 +0530

    [charvi]Added code

---
+++ ControlFlows/Level-1/CountNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CountNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter countdown start number: ");
+        int counter = scanner.nextInt();
+
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
+        scanner.close();
+    }
+}
