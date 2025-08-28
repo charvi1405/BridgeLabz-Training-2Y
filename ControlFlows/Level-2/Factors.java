ef9873d2065d0ec4cbb6aaa903a8a3ff1a9e791b
Charvi Mittal <charvimittal304@gmail.com>
2025-08-28 21:25:59 +0530

    [charvi] Added code

---
+++ ControlFlows/Level-2/Factors.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        if (number < 1) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            System.out.print("Factors of " + number + " are: ");
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+        }
+        scanner.close();}}
