80973e35d2e902ae41de7a51b1d74c4a5a799f08
Charvi Mittal <charvimittal304@gmail.com>
2025-09-11 11:17:30 +0530

    [charvi] refacted code

--- Programming-Elements/Level-1/KilometerToMile.java
+++
@@ -1,9 +0,0 @@
-public class KilometerToMile {
-    public static void main(String[] args) {
-        double kilometer=10.8;
-        double miles=1.6* kilometer;
-        System.out.println(" The distance "+ kilometer +" km in miles is "+miles);
-
-
-    }
-}
